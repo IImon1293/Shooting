127dad0ae5febf19d35205da600a5926e47fd2b6
Yuemon <kuwayuta221293emon@gmail.com>
2025-12-05 11:56:53 +0900

    Player(円)を表示

--- src/shooting/Shooting.java
+++ src/shooting/Shooting.java
@@ -4,13 +4,16 @@ package shooting;
 import javafx.application.Application;
 import javafx.stage.Stage;
 // レイアウト、コントロール
-import javafx.scene.control.ComboBox;	// Start, Pause, Reset, Exit
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;	// player
 
 public class Shooting extends Application{
+	// 構造
+	VBox root;
 	// キャラクター
 	Circle player;
-	ComboBox<String> menu;
 	
 	@Override
 	public void start(Stage stage) throws Exception {	// 例外処理
@@ -18,6 +21,13 @@ public class Shooting extends Application{
 		stage.setWidth(800);
 		stage.setHeight(600);
 		
+		player = new Circle(400, 500, 35); // X, Y, 半径
+		
+		// 表示
+		Pane root = new Pane();	// レイアウトコンテナ
+		root.getChildren().addAll(player);
+		Scene scene = new Scene(root, 300, 200);
+		stage.setScene(scene);
 		stage.show();
 	}
 	
