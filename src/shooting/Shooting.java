77d4b52488caeb45b355a0e702a83478e45e3da7
Yuemon <kuwayuta221293emon@gmail.com>
2025-12-11 20:14:57 +0900

    壁抜け防止 ただし初期設定の画面幅のみ ←できなくなった

--- src/shooting/Shooting.java
+++ src/shooting/Shooting.java
@@ -6,6 +6,7 @@ import javafx.stage.Stage;
 // レイアウト、コントロール
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.ComboBox;
 import javafx.scene.shape.Circle; // player
 // キーイベント
 import javafx.scene.input.KeyEvent;
@@ -27,6 +28,8 @@ public class Shooting extends Application {
 	// プレイヤー
 	Circle player;
 	int[] playerPlace = new int[2]; // 0 → X, 1 → Y
+	int playerRad;
+	// キーのフラグ
 	boolean[] keyFlag = new boolean[4]; // W, S, A, D
 
 	@Override
@@ -35,9 +38,15 @@ public class Shooting extends Application {
 		stage.setWidth(WINDOW_WIDTH);
 		stage.setHeight(WINDOW_HEIGHT);
 
+		// メニュー
+		ComboBox<String> gameMenuCmb = new ComboBox<>(); // ゲームメニュー（start, exit, pause, reset）
+		gameMenuCmb.getItems().addAll("Start", "Pause", "Reset", "Exit");
+
 		playerPlace[X] = 400;
 		playerPlace[Y] = 500;
-		player = new Circle(playerPlace[X], playerPlace[Y], 35); // X, Y, 半径
+		playerRad = 35;
+
+		player = new Circle(playerPlace[X], playerPlace[Y], playerRad); // X, Y, 半径
 
 		// ゲームループ
 		new AnimationTimer() {
@@ -52,7 +61,7 @@ public class Shooting extends Application {
 
 		// 表示
 		Pane root = new Pane(); // レイアウトコンテナ
-		root.getChildren().addAll(player);
+		root.getChildren().addAll(player, gameMenuCmb);
 		Scene scene = new Scene(root, 300, 200);
 		// キーイベント
 		scene.setOnKeyPressed(event -> doKeyAction(event));
@@ -72,16 +81,16 @@ public class Shooting extends Application {
 		moveSpeed[X] = 7;
 		moveSpeed[Y] = 5;
 
-		if (keyFlag[W]) {
+		if (keyFlag[W] && playerRad < playerPlace[Y]) { // プレイヤー全体が画面に収まるようにする
 			playerPlace[Y] -= moveSpeed[Y];
 		}
-		if (keyFlag[S]) {
+		if (keyFlag[S] && playerPlace[Y] < WINDOW_HEIGHT - playerRad) {
 			playerPlace[Y] += moveSpeed[Y];
 		}
-		if (keyFlag[A]) {
+		if (keyFlag[A] && playerRad < playerPlace[X]) {
 			playerPlace[X] -= moveSpeed[X];
 		}
-		if (keyFlag[D]) {
+		if (keyFlag[D] && playerPlace[X] < WINDOW_WIDTH - playerRad) {
 			playerPlace[X] += moveSpeed[X];
 		}
 	}
