09a9aae60e35ff820244b19ba35df6ce554bb935
IImon <kuwayuta221293emon@gmail.com>
2026-01-21 05:06:38 +0900

    とりあえず雑にcommit, Push

--- src/shooting/EnemyManager.java
+++ src/shooting/EnemyManager.java
@@ -1,16 +1,35 @@
 package shooting;
 
+import java.sql.Time;
 import java.util.ArrayList;
+
+import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class EnemyManager {
-  private TimerManager timerManager; // タイマーの制御と同期させる
-
+  // インスタンス
   private Timeline enemyTimeline = new Timeline();
   private ArrayList<Rectangle> enemy = new ArrayList<>();
 
-  public EnemyManager() {
+  // 敵を生成する間隔
+  private
+
+  public EnemyManager(TimerManager timer) {
+    if (timer.getIsTimerStarted() && timer.getIsPause()) { // タイマーが進行しているとき
+      enemyTimeline.play();
+    } else {
+      enemyTimeline.pause();
+    }
+
+    enemyTimeline = new Timeline(new KeyFrame(Duration.millis(1000), new EventHandler<ActionEvent>() {
+      public void handle(ActionEvent actionEvent) {
+
+      }
+    }));
   }
 
   private void spawnEnemy() {
